4f5df0e2a0445a6e969b355a77a5215d6fe92425
mdstev1 <60158456+mdstev1@users.noreply.github.com>
2020-02-06 12:20:00 -0700

    Update unitconverter.js

--- unitconverter.js
+++ unitconverter.js
@@ -1,7 +1,51 @@
-
-function myFunc(){
-    alert("You clicked it!")
-    var myDIV = document.getElementById('txt')
-    myDIV.innerHTML="<b>New Text!</b>"
-  }
+function Calc() {
+ var ft=document.getElementById("number").value
+  if(document.getElementById("inches").checked==true){
+    ft=ft/12;
+        }
+    else if(document.getElementById("feet").checked==true){
+    ft=ft;
+        }
+    else if(document.getElementById("yards").checked==true){
+    ft=ft*3;
+        }
+    else if(document.getElementById("miles").checked==true){
+    ft=ft*5280;
+        }
+    else if(document.getElementById("meters").checked==true){
+    ft=ft*3.28084;
+        }
+    else if(document.getElementById("kilometers").checked==true){
+    ft=ft*3280.84;
+        }  
+ var ans=ft
+  
+  if(document.getElementById("inches2").checked==true){
+    ans=ans*12;
+        }
+    else if(document.getElementById("feet2").checked==true){
+    ans=ans;
+        }
+    else if(document.getElementById("yards2").checked==true){
+    ans=ans/3;
+        }
+    else if(document.getElementById("miles2").checked==true){
+    ans=ans/5280;
+        }
+    else if(document.getElementById("meters2").checked==true){
+    ans=ans/3.28084;
+        }
+    else if(document.getElementById("kilometers2").checked==true){
+    ans=ans/3280.84;
+        } 
+  
+  if(ans%1==0){
+    ans=ans
+        }
+    else {
+    ans=ans.toFixed(4)
+        }
+  var div = document.getElementById('answer')
+  div.innerHTML=ans
+}
 
