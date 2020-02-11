19616a88ae3d53dcee9299cf8e4d587c7f2af102
mdstev1 <60158456+mdstev1@users.noreply.github.com>
2020-02-11 14:51:24 -0700

    Update manning.js

--- manning.js
+++ manning.js
@@ -1,55 +1,23 @@
 function Calc() {
- var ft=document.getElementById("number").value
-  if(document.getElementById("inches").checked==true){
-    ft=ft/12;
+ var n=document.getElementById("n").value
+ var R=document.getElementById("R").value
+ var S=document.getElementById("S").value
+ if(document.getElementById("US").checked==true){
+    var V=1.49/n * R**(2/3) * S**(0.5);
+    }
+   
+    else if(document.getElementById("SI").checked==true){
+    var V=(1/n) * R**(2/3) * S**(0.5);
         }
-    else if(document.getElementById("feet").checked==true){
-    ft=ft;
-        }
-    else if(document.getElementById("yards").checked==true){
-    ft=ft*3;
-        }
-    else if(document.getElementById("miles").checked==true){
-    ft=ft*5280;
-        }
-    else if(document.getElementById("meters").checked==true){
-    ft=ft*3.28084;
-        }
-    else if(document.getElementById("kilometers").checked==true){
-    ft=ft*3280.84;
-        }  
- var ans
-  
-  if(document.getElementById("inches2").checked==true){
-    ans=ft*12;
-        }
-    else if(document.getElementById("feet2").checked==true){
-    ans=ft;
-        }
-    else if(document.getElementById("yards2").checked==true){
-    ans=ft/3;
-        }
-    else if(document.getElementById("miles2").checked==true){
-    ans=ft/5280;
-        }
-    else if(document.getElementById("meters2").checked==true){
-    ans=ft/3.28084;
-        }
-    else if(document.getElementById("kilometers2").checked==true){
-    ans=ft/3280.84;
-        } 
-  
-  if(ans%1==0){
+
+  if(V%1==0){
     ans=ans
         }
     else {
-    ans=ans.toFixed(4)
+    V=V.toFixed(4)
         }
-  var div = document.getElementById('answer')
-  div.innerHTML=ans
+  
+  var ans = document.getElementById('answer')
+  ans.innerHTML=V
 }
-
-function Pic() {
-var pic = "ThumbsUpChuck.gif"
-  document.getElementById('picture').src = pic.replace('90x90', '225x225');
-}
+  
