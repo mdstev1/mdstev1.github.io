2cfa5a0d94adbc5712bd9f1fa6f98f3b4b5c28b3
mdstev1 <60158456+mdstev1@users.noreply.github.com>
2020-02-13 13:00:40 -0700

    Update manning.js

--- manning.js
+++ manning.js
@@ -1,3 +1,16 @@
+function ADDunits() {
+    var lunits = document.getElementById('lunits')
+    var sunits = document.getElementById('sunits')
+  if(document.getElementById("US").checked==true){
+    lunits.innerHTML="[ft]"
+    sunits.innerHTML="[ft/ft]"
+    }
+  else if(document.getElementById("SI").checked==true){
+    lunits.innerHTML="[m]"
+    sunits.innerHTML="[m/m]"
+        }
+}
+
 function Calc() {
  var n=document.getElementById("n").value
  var R=document.getElementById("R").value
