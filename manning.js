fab4f252414327794ce4016e430594cd6804709c
mdstev1 <60158456+mdstev1@users.noreply.github.com>
2020-02-13 13:09:12 -0700

    Update manning.js

--- manning.js
+++ manning.js
@@ -12,16 +12,19 @@ function ADDunits() {
 }
 
 function Calc() {
- var n=document.getElementById("n").value
- var R=document.getElementById("R").value
- var S=document.getElementById("S").value
+ var n=document.getElementById("n").value;
+ var R=document.getElementById("R").value;
+ var S=document.getElementById("S").value;
+ var vunits = document.getElementById('vunits');
  if(document.getElementById("US").checked==true){
     var V=1.49/n * R**(2/3) * S**(0.5);
+     vunits.innerHTML="[ft/s]";
     }
    
-    else if(document.getElementById("SI").checked==true){
+ else if(document.getElementById("SI").checked==true){
     var V=(1/n) * R**(2/3) * S**(0.5);
-        }
+    vunits.innerHTML="[m/s]";
+    }
 
   if(V%1==0){
     ans=ans
