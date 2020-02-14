ae38a9e0a0a9ccc5ccac508d3608a20340f9d230
mdstev1 <60158456+mdstev1@users.noreply.github.com>
2020-02-13 19:27:11 -0700

    Update leafletmap.js

--- leafletmap.js
+++ leafletmap.js
@@ -1,3 +1,4 @@
+ /*
  var map = L.map('map').setView([51.505, -0.09], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -8,7 +9,7 @@
         L.marker([51.5, -0.09]).addTo(map)
         .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
         .openPopup();
-
+*/
 
 
 
